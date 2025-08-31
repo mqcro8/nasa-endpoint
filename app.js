f585c8704cc032d37d6d24c173006d51588af823
maqcro8 <miguel.ceballos.a@hotmail.com>
2025-08-31 16:28:54 -0600

    Fix ans undefined error

--- app.js
+++ app.js
@@ -10,42 +10,27 @@ app.use((err, req, res, next) => {
 app.get('/EONET', async (req, res) => {
     const lan = req.query.lan;
 
-    try {
-        const { ans } = await idkFu(lan);
-        if(!ans) return res.status(400).send({error: {message:"Server error"}});
-    } catch (error) {
-        return res.status(400).send({error: {message:"Server error"}});
-    }
-    
+    const { ans } = await idkFu(lan);
+    if(!ans) return res.status(400).send({error: {message:"Server error"}});
+
     return res.status(200).send(ans);
 })
 
 app.get('/NeoWs', async (req, res) => {
     const lan = req.query.lan;
 
-    try{
-        const { ans } = await idkFu2(lan);
-        if(!ans) return res.status(400).send({error: {message:"Server error"}});
-    }
-    catch(error){
-        if(!ans) return res.status(400).send({error: {message:"Server error"}});
-    }
-    
-    
+    const { ans } = await idkFu2(lan);
+    if(!ans) return res.status(400).send({error: {message:"Server error"}});
+
     return res.status(200).send(ans);
 })
 
 app.get('/APOD', async (req, res) => {
     const lan = req.query.lan;
 
-    try{
-        const { ans } = await idkFu3(lan);
-        if(!ans) return res.status(400).send({error: {message:"Server error"}});
-    }
-    catch(error){
-        if(!ans) return res.status(400).send({error: {message:"Server error"}});
-    }
-
+    const { ans } = await idkFu3(lan);
+    if(!ans) return res.status(400).send({error: {message:"Server error"}});
+ 
     return res.status(200).send(ans);
 })
 
